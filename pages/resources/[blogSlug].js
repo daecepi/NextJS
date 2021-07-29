807d6dc042cb2badb598fe3ce0806cb8c6d003b5
daecepi <daecepi@gmail.com>
2021-07-28 22:56:40 -0500

    Ädds baseline for the blog pages

--- pages/resources/[blogSlug].js
+++ pages/resources/[blogSlug].js
@@ -1,10 +1,37 @@
 import { useRouter } from 'next/router'
+import { getAllPostsWithSlug, getBlogEntry } from '../../lib/api';
 
-const Post = () => {
-  const router = useRouter()
-  const { blogSlug } = router.query
+const Post = ({entry}) => {
+  // const router = useRouter()
+  // const { blogSlug } = router.query
 
-  return <p>Post: {blogSlug}</p>
+  return (
+  <>
+    <p>Post: entry.title</p>
+    {/*entry.landingBlocks.map((block)=>{
+            console.log(block)
+            return (
+                <p>{ block.typeHandle }</p>
+            )
+        })*/}
+  </>
+  )
 }
 
 export default Post
+
+export async function getServerSideProps({params}) {
+  const entry = await getBlogEntry(params);
+
+  if (!entry) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+        entry
+    }, // will be passed to the page component as props
+  }
+}
