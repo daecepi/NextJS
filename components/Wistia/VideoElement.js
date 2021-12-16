b77a7e322efc9920ce34ef80bc5c1d383db46169
David <daecepi@gmail.com>
2021-12-16 12:23:28 -0500

    Adds wistia library to deffering

--- components/Wistia/VideoElement.js
+++ components/Wistia/VideoElement.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 const VideoElement = ({ url }) => {
 	useEffect(() => {
 		const wistiaLibrary = document.createElement("script");
-		wistiaLibrary.src = "https://fast.wistia.com/embed/medias/videolink.jsonp";
+		wistiaLibrary.src = "https://fast.wistia.com/assets/external/E-v1.js";
 		wistiaLibrary.defer = true;
 
 		const wistiaScript = document.createElement("script");
