0401d41e3e969ba2b04f216664dc269c377be662
jashiacopper <jshia@copper.com>
2021-12-20 14:38:41 -0800

    points directly to resources

--- components/Navigation/NavigationDefault.js
+++ components/Navigation/NavigationDefault.js
@@ -333,7 +333,7 @@ const NavigationDefault = ({ buttons }) => {
 						iconHeight: 28,
 						description:
 							"News, stories and latest thinking on sales, marketing and more.",
-						url: "/blog",
+						url: "/resources",
 						tag: "",
 						colPos: 3,
 					},
