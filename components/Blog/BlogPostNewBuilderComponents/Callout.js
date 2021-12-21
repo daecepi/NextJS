b1f87e828079c1f34000e0c84858fa1d8f7acdb0
David <daecepi@gmail.com>
2021-12-21 15:23:22 -0500

    Fix to the callout component

--- components/Blog/BlogPostNewBuilderComponents/Callout.js
+++ components/Blog/BlogPostNewBuilderComponents/Callout.js
@@ -1,5 +1,10 @@
 const Callout = ({ copy }) => {
-	return <div className="c-resource-callout col-md-10">{copy}</div>;
+	return (
+		<div
+			className="c-resource-callout col-md-10"
+			dangerouslySetInnerHTML={{ _html: copy }}
+		></div>
+	);
 };
 
 export default Callout;
