19f5bdb8f5db631d415aacfba2490ec5ee25b95b
jashiacopper <jshia@copper.com>
2021-12-23 00:05:34 -0800

    removes preloaded fonts

--- pages/_document.js
+++ pages/_document.js
@@ -10,38 +10,7 @@ class MyDocument extends Document {
 	render() {
 		return (
 			<Html>
-				<Head>
-					<link
-						rel="preload"
-						href="/fonts/Graphik-Medium-Web.woff2"
-						as="font"
-						type="font/woff2"
-						crossorigin
-					/>
-					<link
-						rel="preload"
-						href="/fonts/Graphik-Regular-Web.woff2"
-						as="font"
-						type="font/woff2"
-						crossorigin
-					/>
-					<link
-						rel="preload"
-						href="/fonts/Graphik-Light-Web.woff2"
-						as="font"
-						type="font/woff2"
-						crossorigin
-					/>
-					<link
-						href="https://fonts.googleapis.com/icon?family=Material+Icons&display=swap"
-						rel="stylesheet"
-					/>
-					<link
-						href="https://fonts.googleapis.com/css?family=Roboto+Mono:300,500,700&display=swap"
-						rel="stylesheet"
-					/>
-					<link href="https://rsms.me/inter/inter.css" rel="stylesheet" />
-				</Head>
+				<Head />
 				<body>
 					<Main />
 					<NextScript />
