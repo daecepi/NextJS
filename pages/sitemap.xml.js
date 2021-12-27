1a8f6e5b6574c9e8ca03ee10a1b848abea9c86eb
David <daecepi@gmail.com>
2021-12-27 15:49:25 -0500

    Adds property security, improves the date field from wich the date is taken and removes updater from XML till approval in Sitemap functionality

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -7,15 +7,19 @@ export const getServerSideProps = async ({ res }) => {
 	// Urls fetching
 	const sitemapEntries = await getSitemapUrls();
 
+	// Schema printing (add <changefreq>monthly</changefreq> if Google policies accept it)
 	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${sitemapEntries
 			.map((entry) => {
 				return `
           <url>
-            <loc>${entry.url}</loc>
-            <lastmod>${new Date(entry.postDate).toISOString()}</lastmod>
-            <changefreq>monthly</changefreq>
+            <loc>${entry?.url || ""}</loc>
+            <lastmod>${
+							new Date(
+								entry?.dateUpdated || new Date().toISOString()
+							).toISOString() || ""
+						}</lastmod>
             <priority>1.0</priority>
           </url>
         `;
